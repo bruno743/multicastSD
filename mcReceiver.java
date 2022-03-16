b9719be254df3f451e5eb7b20ba6279ca92c931e
bruno743 <brunofelipe743@gmail.com>
2022-03-15 22:56:13 -0300

    correcoes

--- mcReceiver.java
+++ mcReceiver.java
@@ -2,40 +2,86 @@ import java.net.*;
 import java.nio.*;
 import java.io.*;
 import java.util.Scanner;
+import javax.script.*;
 
 public class MulticastReceiver extends Thread {
     protected MulticastSocket socket = null;
     protected byte[] buf = new byte[256];
 
-    public void mcRun(String ipA) throws IOException {
+    private DatagramSocket socket_;
+    private byte[] buf_;
+
+    private String message;
+    private String thisMaq = "1"; // um valor diferente para cada maquina
+
+    public void mcRun(String add) throws IOException {
         socket = new MulticastSocket(4446);
         // ip --> "224.0.0.0"
-        InetAddress group = InetAddress.getByName(ipA);
+        InetAddress group = InetAddress.getByName(add);
         socket.joinGroup(group);
+        
+        Boolean test = true;
+
         while (true) {
+            System.out.println("Aguardando...");
             DatagramPacket packet = new DatagramPacket(buf, buf.length);
             socket.receive(packet);
             String received = new String(packet.getData(), 0, packet.getLength());
-            try {
-                ScriptEngineManager factory = new ScriptEngineManager();
-                ScriptEngine engine = factory.getEngineByName("JavaScript");
-                System.out.println (obj);
+            System.out.println(received);
+
+            if (received.length() > 1) {
+                message = received;
+
+                buf = thisMaq.getBytes();
+                DatagramPacket pack = new DatagramPacket(buf_, buf_.length, group, 4446);
+
+                socket_ = new DatagramSocket();
+                socket_.send(pack);
+                socket_.close();
+            } else {
+                DatagramPacket p = new DatagramPacket(buf, buf.length);
+
+                socket.receive(p);
+                String r = new String(p.getData(), 0, p.getLength());
+                
+                if(Integer.parseInt(thisMaq) > Integer.parseInt(r)) {test = false;}
+                continue;
             }
-            catch(Exception e) {
-                System.out.println("ERROR!");
+
+            if(test){
+                try {
+                    ScriptEngineManager factory = new ScriptEngineManager();
+                    ScriptEngine engine = factory.getEngineByName("JavaScript");
+                    Object obj = engine.eval(message);
+
+                    buf_ = (thisMaq + ": " + obj.toString()).getBytes();
+                    DatagramPacket packet_ = new DatagramPacket(buf_, buf_.length, group, 4447);
+
+                    socket_ = new DatagramSocket();
+                    socket_.send(packet_);
+                    socket_.close();
+
+                    System.out.println(obj);
+                } catch (Exception e) {
+                    System.out.println("ERROR!");
+                }
             }
-            if ("end".equals(received)) {
+
+            if("end".equals(received)){
                 break;
             }
         }
+        
         socket.leaveGroup(group);
         socket.close();
     }
 
    public static void main(String[] args) throws IOException {
        MulticastReceiver mr=new MulticastReceiver();
-       System.out.println("Informe o IP: ");
-       String ipAddr = scan.nextLine();
-       mr.mcRun(ipAddr);
+       Scanner scan = new Scanner(System.in);
+       System.out.print("Informe o IP do grupo multicast: ");
+       String add = scan.nextLine();
+       //String add = "224.0.0.0";
+       mr.mcRun(add);
    }
 }
--- mcSender.java
+++ mcSender.java
@@ -8,23 +8,42 @@ public class MulticastPublisher {
     private InetAddress group;
     private byte[] buf;
 
-    public void multicast(String multicastMessage, String ipA) throws IOException {
+    public class MR extends Thread {
+        protected MulticastSocket socket_ = null;
+        protected byte[] buf_ = new byte[256];
+    }
+
+    public void multicast(String multicastMessage, String add) throws IOException {
         socket = new DatagramSocket();
         // ip --> "224.0.0.0"
-        group = InetAddress.getByName(ipA);
+        group = InetAddress.getByName(add);
         buf = multicastMessage.getBytes();
         DatagramPacket packet = new DatagramPacket(buf, buf.length, group, 4446);
         socket.send(packet);
+
+        System.out.println("Aguardando...");
+        MR mr = new MR();
+        mr.socket_ = new MulticastSocket(4447);
+        mr.socket_.joinGroup(group);
+        DatagramPacket packet_ = new DatagramPacket(mr.buf_, mr.buf_.length, group, 4446);
+        mr.socket_.receive(packet_);
+        String received = new String(packet_.getData(), 0, packet_.getLength());
+        System.out.println(received);
+
+        mr.socket_.leaveGroup(group);
+        mr.socket_.close();
+
         socket.close();
     }
 
     public static void main(String[] args) throws IOException {
        MulticastPublisher mp = new MulticastPublisher();
        Scanner scan = new Scanner(System.in);
-       System.out.println("Informe o IP: ");
-       String ipAddr = scan.nextLine();
-       System.out.println("Escreva uma expressao: ");
+       System.out.print("Informe o IP do grupo multicast: ");
+       String add = scan.nextLine();
+       //String add = "224.0.0.0";
+       System.out.print("Escreva uma expressao: ");
        String op = scan.nextLine();
-       mp.multicast(op, ipAddr);
+       mp.multicast(op, add);
    }
 }
